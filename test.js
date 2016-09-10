aa491fa4309f6552f033e5c6bb3ba4db69b8c1dc
Def <def@hai.li>
2016-09-10 16:49:57 +0800

    edit test

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-var zip_array = require("./index")
-zip_array([[1,2], [10, 20], [100, 200]], function(array) {
-    console.log(
-        JSON.stringify(array)
-        ===
-        "[[1,10,100],[2,20,200]]"
-        ? "[pass]" : "[fail]",
+var zip_array = require("./index");
+var array = zip_array([[1,2], [10, 20], [100, 200]], function(v) {return v})
+console.log(
+    JSON.stringify(array)
+    ===
+    "[[1,10,100],[2,20,200]]"
+    ? "[pass]" : "[fail]",
 
-        "([[1, 2], [10, 20], [100, 200]], callback)",
-        "=>",
-        "callback([[1, 10, 100], [2, 20, 200]])"
-    )
-})
+    "([[1, 2], [10, 20], [100, 200]], callback)",
+    "=>",
+    "callback([[1, 10, 100], [2, 20, 200]])"
+)
